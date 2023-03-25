5c0cb8486ff2c587715fc65724a3cb536586ecd8
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-03-25 09:59:08 +0100

    Documentation for login-form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -10,6 +10,7 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 
 import { Router } from '@angular/router';
 
+/** @Component decorator to tell Angular that the class right below is a component.*/ 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -17,12 +18,22 @@ import { Router } from '@angular/router';
 })
 export class UserLoginFormComponent implements OnInit {
   // The userData object will then be passed into the API call in the userLogin function.
+  /**
+   * The @loginData object will then be passed into the API call in the registerUser function.
+   * @loginrData object contains: @Username (required), @Password (required)
+   */
   @Input() loginData = {
     Username: '',
     Password: '',
   };
 
-  // constructor is used to set dependencies. Constructor arguments then will be avaliable through "this" method
+  /**
+   * @constructor is used to set dependencies. Constructor arguments then will be avaliable through "this" method
+   * @param dialogRef to call dialog with login inputs
+   * @param FetchApiData to use functions to make API call
+   * @param router to navigate the user to welcome MovieCard after logging in
+   * @param snackBar to show the message, that user has successfuly loged in
+   */
   constructor(
     public FetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
@@ -30,15 +41,18 @@ export class UserLoginFormComponent implements OnInit {
     public router: Router
   ) {}
 
-  // The ngOnInit method is called once the component has received all its inputs
-  // (all its data-bound properties) from the calling component (user)
+  /**
+   * This function calls specified methods automatically straight after Component was mounted
+   */
   ngOnInit(): void {}
 
-  // This function is responsible for sending the form to the backend
-  // Function for sending the form inputs to the backend to login user
-  // If success, set the localstorage with user and token
-  // if fails, snakBar shows error message
 
+  /**
+   * This is the function responsible for sending the form inputs to the backend API to login user
+   * @function registerUser
+   * If success, set the localstorage with user and token
+   * if fails, snakBar shows error message
+   */
   loginUser(): void {
     this.FetchApiData.userLogin(this.loginData).subscribe({
       // if success, localStorage setItem token and user
